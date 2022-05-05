99f48a2573505b5298a3ccc6843c08c1dc29830e
Morcego <vinicius.templario@gmail.com>
2022-05-05 17:48:38 -0300

    Replicando o diretorio para refatorar o projeto

---
+++ app_carrinho_compras_b/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use App\CarrinhoCompra;
+
+$carrinho1 = new CarrinhoCompra();
+
+
+echo 'Valor Total: '.$carrinho1->exibirValorTotal();
+echo "<br />";
+/*
+$carrinho1->adicionarItem('Biscicleta', 750.10);
+$carrinho1->adicionarItem('Geladeira', 2050.16);
+$carrinho1->adicionarItem('Tapete', 50.12);
+*/
+
+
+print_r($carrinho1->exibirItens());
+echo "<br />";
+echo 'Valor Total Recalculado: '.$carrinho1->exibirValorTotal();
+echo "<br />";
+echo 'Status: '.$carrinho1->exibirStatus();
+echo "<br />";
+if($carrinho1->validarCarrinho()){
+    echo 'Pedido realizado com sucesso!';
+}else{
+    echo 'Erro ao confirmar o pedido. Carrinho vazio!';
+}
+echo "<br />";
+echo 'Status: '.$carrinho1->exibirStatus();
+
