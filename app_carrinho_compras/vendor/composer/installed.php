7ed1e29f94f39ac98c42df3f160c481f3c449df4
Morcego <vinicius.templario@gmail.com>
2022-05-05 15:05:45 -0300

    Adicionando projeto

---
+++ app_carrinho_compras/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09891948c915c34dc1e936f3e63ea042bb689bc8',
+        'name' => 'vinicius/app_carrinho_compras',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vinicius/app_carrinho_compras' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09891948c915c34dc1e936f3e63ea042bb689bc8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
