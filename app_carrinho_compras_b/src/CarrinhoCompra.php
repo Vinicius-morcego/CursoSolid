b1c69385a82eca38046fb7900f558f85a163c40c
Morcego <vinicius.templario@gmail.com>
2022-05-06 09:42:22 -0300

    Refatorando o código

--- app_carrinho_compras_b/src/CarrinhoCompra.php
+++ app_carrinho_compras_b/src/CarrinhoCompra.php
@@ -1,53 +1,30 @@
 <?php
 
-namespace App;
+namespace src\Item;
 
 class CarrinhoCompra{
    //atributos
    private $itens;
-   private $status;
-   private $valorTotal;
+  
 
    //metodos
    public function __construct(){
-    $this->itens = [];
-    $this->status = 'aberto';
-    $this->valorTotal = 0;
+       $this->itens = [];
 
    }
 
-   public function exibirItens(){
+   public function getItens(){
        return $this->itens;
    }
 
-   public function adicionarItem(string $item, float $valor){
-    array_push($this->itens, [$item, $valor]);
-       $this->valorTotal += $valor;
-       return true;
+   public function adicionarItem(Item $item){
+        array_push($this->itens, $item);
+        return true;
    }
 
-   public function exibirValorTotal(){
-       return $this->valorTotal;
-   }
-
-   public function exibirStatus(){
-      $this->status;
-   }
-
-   public function confirmarPedido(){
-        if($this->validarCarrinho)   {
-            $this->status = 'confirmado';
-            $this->enviarEmailConfirmacao();
-            return true;
-        }
-        return false;
-   }
-
-   public function enviarEmailConfirmacao(){
-    echo '<br>...Enviar email confirmação...</br>';
-}
-
-    public function validarCarrinho(){
+   public function validarCarrinho(){
         return count($this->itens) > 0;
-    }
+   }
+
+   
 }
